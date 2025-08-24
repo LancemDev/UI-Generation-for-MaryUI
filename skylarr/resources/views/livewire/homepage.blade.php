980039393520325772070e5309e1cf78212bf920
LancemDev <munyaolance1@gmail.com>
2025-08-25 01:31:19 +0300

    :art: style: Update button colors in login and registration modals for improved visibility

--- skylarr/resources/views/livewire/homepage.blade.php
+++ skylarr/resources/views/livewire/homepage.blade.php
@@ -7,27 +7,27 @@
             >
 
             <!-- Login Modal -->
-            <x-modal wire:model="loginModal" class="backdrop-blur" style="color: rgb(236, 82, 40)">
+            <x-modal wire:model="loginModal" class="backdrop-blur">
                 <x-header title="Login to SKYLARR" />
                 <x-form wire:submit="loginUser">
                     <x-input wire:model="email" label="Continue with email" inline icon="o-envelope" />
                     <x-input wire:model="password" type="password" label="Password" inline icon="o-lock-closed" />
 
                     <x-slot:actions>
-                        <x-button type="submit" label="Login" class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" spinner="loginUser" />
+                        <x-button type="submit" label="Login" class="rounded-md bg-green-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" spinner="loginUser" />
                     </x-slot:actions>
                 </x-form>
             </x-modal>
 
             <!-- Register -->
-            <x-modal wire:model="registerModal" style="color: rgb(236, 82, 40)" class="backdrop-blur">
+            <x-modal wire:model="registerModal" class="backdrop-blur">
                 <x-header title="Create a new account" />
                 <x-form wire:submit="register">
                     <x-input wire:model="name" label="Name" inline icon="o-user" />
                     <x-input wire:model="email" label="Email" inline icon="o-envelope" />
                     <x-input wire:model="password" type="password" label="Password" inline icon="o-lock-closed" />
                     <x-slot:actions>
-                        <x-button type="submit" label="Register" class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" spinner="register" />
+                        <x-button type="submit" label="Register" class="rounded-md bg-green-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-700 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" spinner="register" />
                     </x-slot:actions>
                 </x-form>
             </x-modal>
