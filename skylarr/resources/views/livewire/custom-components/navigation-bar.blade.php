62c865fc8e632451293ad06f5cadbe8db9585820
LancemDev <munyaolance1@gmail.com>
2025-09-29 17:25:28 +0300

    :art: feat(ui): Add logout and settings functionality to navigation bar with dropdown menu

--- skylarr/resources/views/livewire/custom-components/navigation-bar.blade.php
+++ skylarr/resources/views/livewire/custom-components/navigation-bar.blade.php
@@ -18,7 +18,10 @@
     <x-theme-toggle />
     <x-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm text-base-100/90 hover:text-base-100 hover:bg-secondary" responsive />
     <x-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm text-base-100/90 hover:text-base-100 hover:bg-secondary" responsive />
-    <x-button label="Profile" icon="o-user" link="##" class="btn-ghost btn-sm text-base-100/90 hover:text-base-100 hover:bg-secondary" responsive />
+    <x-dropdown class="btn-ghost btn-sm text-base-100/90 hover:text-base-100 hover:bg-secondary" icon="o-user" right>
+        <x-menu-item title="Logout" icon="o-power" class="text-red-500" wire:click.stop="logout" spinner="logout" />
+        <x-menu-item title="Settings" icon="o-cog-6-tooth" class="text-blue-500" wire:click.stop="settings" spinner="settings" />
+    </x-dropdown>
  </x-slot:actions>
 </x-nav>
 </div>
